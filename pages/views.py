c8e4d9322d79c4395d08871d1e584601a58d7047
sachin023585193 <sachin023585193@gmail.com>
2022-03-12 15:35:10 +0545

    Finnishing up

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
-from operator import mod
-from django.shortcuts import render
-from .models import Team
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.contrib import messages
 from cars.models import Car
+from .models import Team
 
 # Create your views here.
 def home(request):
@@ -35,4 +37,24 @@ def cars(request):
     return render(request,'pages/cars.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        phone = request.POST['phone']
+        message = request.POST['message']
+
+        admin_infos = User.objects.filter(is_superuser=True)
+        admins_email = []
+        for admin_info in admin_infos:
+            admins_email.append(admin_info.email)
+        send_mail(
+            'You have new contact mail regarding '+subject,
+            'Name: ' + name + 'Email: ' + email + 'Phone: ' + phone + 'Message: ' + message,
+            'sachinbh023@gmail.com',
+            admins_email,
+            fail_silently=False,
+        )
+        messages.success(request,'Thank you cor contacting us we will contact you shortly')
+        return redirect('contact')
     return render(request,'pages/contact.html')
